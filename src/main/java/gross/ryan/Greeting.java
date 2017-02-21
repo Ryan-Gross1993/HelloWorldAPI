ba5b976c88aec348ba4731d7368f815141e8aa84
Ryan Gross <ryan@Ryans-MacBook-Pro.local>
2017-02-21 16:52:46 -0500

    Base program

---
+++ src/main/java/gross/ryan/Greeting.java
@@ -0,0 +1,22 @@
+package gross.ryan;
+
+/**
+ * Created by ryan on 2/21/17.
+ */
+public class Greeting {
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
